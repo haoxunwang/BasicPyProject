bea887c01f28dcff0b597adfc9b5a5ee6444f26d
wanghaoxun <wanghaoxun@guazi.com>
2018-04-15 16:54:10 +0800

    first commit

---
+++ example/HelloPy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Desc    :
+# @Time    : 18/4/15 下午4:49
+# @Author  : Nelson
+# @Contact : haoxunwang525@gmail.com
+# @Site    : nelsonblog.me
+
+print("hello, python3.x")
